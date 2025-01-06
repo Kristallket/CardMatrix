6418353a5cb8ba33678f8128e7faf8ddb8135756
Kristallket <kirill1103morozov@gmail.com>
2025-01-06 18:05:31 +0800

    легкая поправочка
    заменил текстурку.Главная проблема это передвижение...

--- app/src/main/java/com/example/cardmatrix/MainActivity.java
+++ app/src/main/java/com/example/cardmatrix/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.cardmatrix;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,25 +10,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    ImageView box;
+    ImageView you;
     ImageView plita1;
     ImageView plita2;
 
-    Button button;
+    Button right;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button = findViewById(R.id.button);
-        box = findViewById(R.id.imageView4);
+        right = findViewById(R.id.right);
+        you = findViewById(R.id.you);
         plita1 = findViewById(R.id.plita);
         plita2 = findViewById(R.id.plita2);
 
-        button.setOnClickListener(new View.OnClickListener() {
+
+        right.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                box.layout(plita2.getLeft(),plita2.getTop(),plita2.getRight(),plita2.getBottom());
+                you.layout(plita2.getLeft(),plita2.getTop(),plita2.getRight(),plita2.getBottom());
             }
         });
     }
