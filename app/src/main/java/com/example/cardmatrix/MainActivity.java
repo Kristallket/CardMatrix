a16394b766e947a03c7067b18d7ce06314adc129
Kristallket <kirill1103morozov@gmail.com>
2025-01-19 19:59:25 +0800

    начало

--- app/src/main/java/com/example/cardmatrix/MainActivity.java
+++ app/src/main/java/com/example/cardmatrix/MainActivity.java
@@ -9,26 +9,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    ImageView box;
-    ImageView plita1;
-    ImageView plita2;
 
-    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button = findViewById(R.id.button);
-        box = findViewById(R.id.imageView4);
-        plita1 = findViewById(R.id.plita);
-        plita2 = findViewById(R.id.plita2);
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                box.layout(plita2.getLeft(),plita2.getTop(),plita2.getRight(),plita2.getBottom());
-            }
-        });
     }
 }
